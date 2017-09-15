bd4357ab3e40807dcedca0b361e155db3e0273c9
CASSIANO FICAGNA BARBOSA <ALU201310308@uniritter.local>
2017-09-14 21:18:03 -0300

    1.0

---
+++ application/views/Home.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+</head>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>Home test 1 </p>
+	</div>
+
+	
+</div>
+
+</body>
+</html>
