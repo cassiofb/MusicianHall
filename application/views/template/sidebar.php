1d35119f0c3c26ec72bf528f8664238367ca77cf
William Mangoni <willrpg@hotmail.com>
2018-04-25 13:42:37 -0300

    ajustes de layout no menu latera e na modal do chat

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -2,8 +2,13 @@
     <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
       <ul>
         <li class="active"><a href="<?php echo base_url('home'); ?>"><i class="icon icon-home"></i> <span>Home</span></a> </li>
-        <li><a href="<?php echo base_url('chat'); ?>"><i class="icon icon-comments"></i> <span>Chat</span></a>
-          <a href="#" class="" style="position: absolute; top: 0; right: 0;" data-toggle="modal" data-target="#exampleModal"><i class="icon icon-plus"></i></a></li>
+        <li>
+          <a href="<?php echo base_url('chat'); ?>"><i class="icon icon-comments"></i> <span>Chat</span></a>
+          <a href="#" style="position: absolute; top: 0; right: 0; border-left: 1px solid #555; box-shadow: -1px 0px 0px #222; padding: 10px 0 10px 10px;" 
+            data-toggle="modal" data-target="#exampleModal">
+            <i class="icon icon-plus"></i>
+          </a>
+        </li>
         <li><a href=""<?php echo base_url('Contacts'); ?>""><i class="icon icon-group"></i> <span>Contatos</span></a></li>
       </ul>
     </div>
