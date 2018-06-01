2356c4128efd860d9c8701e53bfdbb261c529ebe
William Mangoni <willrpg@hotmail.com>
2018-06-01 12:52:17 -0300

    ajustes na navbar

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -6,28 +6,28 @@
     <div id="app">
 
       <!--top-Header-menu-->
-      <div id="user-nav" class="navbar navbar-inverse">
+      <div id="user-nav" class="navbar navbar-inverse" data-user='<?php echo json_encode($this->session->userdata()); ?>'>
         <ul class="nav">
-          <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome User {{message}}</span><b class="caret"></b></a>
+          <li  class="dropdown" id="profile-messages" ><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">{{message}} <?php echo explode(' ', $this->session->userdata('user_name'))[0]; ?></span><b class="caret"></b></a>
             <ul class="dropdown-menu">
-              <li><a href="<?php echo base_url('profile') ?>"><i class="icon-user"></i> My Profile</a></li>
+              <li><a href="<?php echo base_url('profile') ?>"><i class="icon-user"></i> {{profile.label_profile}}</a></li>
               <li class="divider"></li>
-              <li><a href="#"><i class="icon-check"></i> My Tasks</a></li>
+              <li><a href="#"><i class="icon-check"></i> {{profile.label_tasks}}</a></li>
               <li class="divider"></li>
-              <li><a href="login"><i class="icon-key"></i> Log Out</a></li>
+              <li><a href="login"><i class="icon-key"></i> {{profile.label_logout}}</a></li>
             </ul>
           </li>
-          <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text">Mensagens</span> <span class="label label-important">5</span> <b class="caret"></b></a>
+          <li class="dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text"> {{messages.label}}</span> <span class="label label-important">5</span> <b class="caret"></b></a>
             <ul class="dropdown-menu">
-              <li><a class="sAdd" title="" href="<?php echo base_url('newmessage') ?>"><i class="icon-plus"></i> new message</a></li>
+              <li><a class="sAdd" title="" href="<?php echo base_url('newmessage') ?>"><i class="icon-plus"></i> {{messages.label_new_message}}</a></li>
               <li class="divider"></li>
-              <li><a class="sInbox" title="" href="<?php echo base_url('messages'); ?>"><i class="icon-envelope"></i> inbox</a></li>
+              <li><a class="sInbox" title="" href="<?php echo base_url('messages'); ?>"><i class="icon-envelope"></i> {{messages.label_inbox}}</a></li>
               <li class="divider"></li>
-              <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> trash</a></li>
+              <li><a class="sTrash" title="" href="#"><i class="icon-trash"></i> {{messages.label_trash}}</a></li>
             </ul>
           </li>
-          <li class=""><a title="" href="<?php echo base_url('settings') ?>"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
-          <li class=""><a title="" href="<?php echo base_url('login/loggout') ?>"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+          <li class=""><a title="" href="<?php echo base_url('settings') ?>"><i class="icon icon-cog"></i> <span class="text">{{messages.label}}</span></a></li>
+          <li class=""><a title="" href="<?php echo base_url('login/loggout') ?>"><i class="icon icon-share-alt"></i> <span class="text">{{messages.label_logout}}</span></a></li>
           <li class=""><a title="" href="www.facebook.com"><i class="icon icon-facebook-sign"></i></a></li>
         </ul>
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,21 @@ Vue.component('todo-item', {
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Cassiano'
+    message: 'Bem vindo',
+    profile: {
+    	label_profile: 'Perfil',
+    	label_tasks: 'Tarefas',
+    	label_logout: 'Sair'
+    },
+    messages: {
+    	label: 'Mensagens',
+    	label_new_message: 'Nova mensagem',
+    	label_inbox: 'Inbox',
+    	label_trash: 'Lixeira'
+    },
+    settings: {
+    	label: 'Configurações',
+    	label_logout: 'Sair'
+    }
   }
 })
